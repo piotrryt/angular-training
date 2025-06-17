4dc3c5d92af2d79348cad540fe3a44fcf6c60022
Your Name <you@example.com>
2025-06-17 10:09:00 +0200

    pierwsze kroki

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "overview",
+    loadComponent: () =>
+      import("./features/overview/overview").then((m) => m.Overview),
+  },
+  {
+    path: "",
+    redirectTo: "overview",
+  },
+  {
+    path: "**",
+    redirectTo: "overview",
+  },
+];
---
+++ project/src/app/features/overview/overview.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-overview",
+  imports: [],
+  template: ` <p>overview works!</p> `,
+})
+export class Overview {}
